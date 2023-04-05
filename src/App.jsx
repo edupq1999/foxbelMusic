4a1c822588114ae0d3ef6b6daa0e35e08c1c97e1
edupq1999 <nivel.17@hotmail.com>
2023-04-05 02:40:18 -0500

    Se agrego el header movil y web con sus respectivs diseños

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import PlayBar from './components/PlayBar'
+import Home from './pages/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <PlayBar></PlayBar>
+      <Home></Home>
+      
     </div>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import logo from "../img/foxbel-music@3x.png"
+import './styles/header.css'
+
+const Header = () => {
+    const toggleLibrary = () => {
+        const library = document.querySelector('.library')
+        library.classList.toggle('option_show')
+        console.log(library)
+    }
+    const togglePlaylist = () => {
+        const library = document.querySelector('.playlist')
+        library.classList.toggle('option_show')
+        console.log(library)
+    }
+  return (
+    <header className='header'>
+        <img className='header_logo' src={logo} alt="logo" />
+        <div className='header_filters'>
+            <ul className="header_filter_container">
+                <h2 onClick={toggleLibrary} className="header_filter_title">Mi Librería</h2>
+                <div className='filter_container library'>
+                    <li className="header_filter_option">Recientes</li>
+                    <li className="header_filter_option">Artistas</li>
+                    <li className="header_filter_option">Álbums</li>
+                    <li className="header_filter_option">Canciones</li>
+                    <li className="header_filter_option">Estaciones</li>
+                </div>
+            </ul>
+            <ul className="header_filter_container">
+                <h2 onClick={togglePlaylist} className="header_filter_title">Playlist</h2>
+                <div className='filter_container playlist'>
+                    <li className="header_filter_option">Metal</li>
+                    <li className="header_filter_option">Para bailar</li>
+                    <li className="header_filter_option">Rock 90s</li>
+                    <li className="header_filter_option">Baladas</li>
+                </div>
+            </ul>
+        </div>
+    </header>
+  )
+}
+
+export default Header
